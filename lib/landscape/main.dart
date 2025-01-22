a672f7a3b5268b04081496e9206c0ffea9d19aac
icannns <ikhsanfn2nd@gmail.com>
2025-01-22 13:19:13 +0700

    kotak

--- lib/landscape/main.dart
+++ lib/landscape/main.dart
@@ -39,108 +39,103 @@ class LoginScreen extends StatelessWidget {
                   end: Alignment.bottomCenter,
                 ),
               ),
-              child: Center(
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2),
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 36.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text.rich(
+                    TextSpan(
+                      text: 'Kelola konten Anda dengan\n',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black54,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'mudah dan efisien!',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF0C21C1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 50.0),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      prefixIcon: Icon(Icons.email_outlined),
+                      hintText: 'Enter your Email',
+                      border: UnderlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.lock_outline),
+                      hintText: 'Enter your Password',
+                      border: UnderlineInputBorder(),
+                      suffixIcon: Icon(Icons.visibility_off),
+                    ),
+                  ),
+                  SizedBox(height: 40.0),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 14.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        backgroundColor: Color(0xFF0C21C1),
+                        elevation: 5,
+                      ),
+                      child: Text(
                         'Login',
                         style: TextStyle(
-                          fontSize: 36.0,
+                          fontSize: 16.0,
                           fontWeight: FontWeight.bold,
-                          color: Colors.black87,
+                          color: Colors.white,
                         ),
                       ),
-                      SizedBox(height: 8.0),
-                      Text.rich(
-                        TextSpan(
-                          text: 'Kelola konten Anda dengan\n',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.black54,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'mudah dan efisien!',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFF0C21C1),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 30.0),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          prefixIcon: Icon(Icons.email_outlined),
-                          hintText: 'Enter your Email',
-                          border: UnderlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(height: 20.0),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          prefixIcon: Icon(Icons.lock_outline),
-                          hintText: 'Enter your Password',
-                          border: UnderlineInputBorder(),
-                          suffixIcon: Icon(Icons.visibility_off),
-                        ),
-                      ),
-                      SizedBox(height: 40.0),
-                      Center(
-                        child: SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              padding: EdgeInsets.symmetric(vertical: 14.0),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50.0),
-                              ),
-                              backgroundColor: Color(0xFF0C21C1),
-                              elevation: 5,
-                            ),
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  Spacer(flex: 3),
+                ],
               ),
             ),
           ),
-          // Bagian Kanan: Logo Hanya
+          // Bagian Kanan: Logo
           Expanded(
             flex: 1,
             child: Container(
               decoration: BoxDecoration(
                 color: Color(0xFF000842), // Background biru gelap
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  bottomLeft: Radius.circular(30.0),
-                ),
               ),
-              child: Center(
-                child: FractionallySizedBox(
-                  widthFactor: 0.5, // Ukuran logo 50% dari lebar kontainer
-                  child: Image.asset(
-                    'assets/jadingetop.png', // Path logo
-                    fit: BoxFit.contain,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FractionallySizedBox(
+                    widthFactor: 0.6, // Menyesuaikan ukuran logo
+                    child: Image.asset(
+                      'assets/jadingetop.png', // Path logo
+                      fit: BoxFit.contain,
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
           ),
