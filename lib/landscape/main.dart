05a2e6d9994e0684e7fc814aee30a70cd120b288
icannns <ikhsanfn2nd@gmail.com>
2025-01-22 13:43:25 +0700

    3333

--- lib/landscape/main.dart
+++ lib/landscape/main.dart
@@ -22,124 +22,129 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          // Bagian Kiri: Form Login
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 40.0),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFDBEAFE), // Biru muda
-                    Color(0xFFFFFFFF), // Putih
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Spacer(flex: 2),
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 36.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
-                  ),
-                  SizedBox(height: 8.0),
-                  Text.rich(
-                    TextSpan(
-                      text: 'Kelola konten Anda dengan\n',
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFDBEAFE), // Biru muda
+              Color(0xFFEAF4FE), // Biru lebih terang
+              Color(0xFFFFFFFF), // Putih
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Row(
+          children: [
+            // Bagian Kiri: Form Login
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Spacer(flex: 2),
+                    Text(
+                      'Login',
                       style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.black54,
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
                       ),
-                      children: [
-                        TextSpan(
-                          text: 'mudah dan efisien!',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF0C21C1),
-                          ),
-                        ),
-                      ],
                     ),
-                  ),
-                  SizedBox(height: 50.0),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      prefixIcon: Icon(Icons.email_outlined),
-                      hintText: 'Enter your Email',
-                      border: UnderlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(Icons.lock_outline),
-                      hintText: 'Enter your Password',
-                      border: UnderlineInputBorder(),
-                      suffixIcon: Icon(Icons.visibility_off),
-                    ),
-                  ),
-                  SizedBox(height: 40.0),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 14.0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        backgroundColor: Color(0xFF0C21C1),
-                        elevation: 5,
-                      ),
-                      child: Text(
-                        'Login',
+                    SizedBox(height: 8.0),
+                    Text.rich(
+                      TextSpan(
+                        text: 'Kelola konten Anda dengan\n',
                         style: TextStyle(
                           fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                          color: Colors.black54,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'mudah dan efisien!',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF0C21C1),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 50.0),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email_outlined),
+                        hintText: 'Enter your Email',
+                        border: UnderlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock_outline),
+                        hintText: 'Enter your Password',
+                        border: UnderlineInputBorder(),
+                        suffixIcon: Icon(Icons.visibility_off),
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 14.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                          backgroundColor: Color(0xFF0C21C1),
+                          elevation: 5,
+                        ),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Spacer(flex: 3),
-                ],
+                    Spacer(flex: 3),
+                  ],
+                ),
               ),
             ),
-          ),
-          // Bagian Kanan: Background dengan Logo di Tengah
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xFF000842), // Background biru gelap
-                borderRadius: BorderRadius.circular(16.0), // Border melengkung
-              ),
-              margin:
-                  EdgeInsets.all(40.0), // Memberikan padding seperti referensi
-              child: Center(
-                child: FractionallySizedBox(
-                  widthFactor: 0.6, // Menyesuaikan ukuran logo
-                  child: Image.asset(
-                    'assets/jadingetop.png', // Path logo
-                    fit: BoxFit.contain,
+            // Bagian Kanan: Kotak dengan Logo
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.all(40.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF000842), // Background biru gelap
+                    borderRadius:
+                        BorderRadius.circular(20.0), // Sudut melengkung
+                  ),
+                  child: Center(
+                    child: FractionallySizedBox(
+                      widthFactor: 0.6, // Menyesuaikan ukuran logo
+                      child: Image.asset(
+                        'assets/jadingetop.png', // Path logo
+                        fit: BoxFit.contain,
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
